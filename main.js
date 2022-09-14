7f90488e2786fa92b0345ee3752a6aefb03f637f
YourFBIAgent <nedzaddonlagic04@gmail.com>
2022-09-14 16:39:37 +0200

    Animation related changes
    Added animations for the streak being broken and
    continued, as well as animations that show which
    box was the correct one and which box was the
    wrong one.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Importing the needed modules
 import { HSL } from "/hslClass.mjs";
+import anime from "animejs";
 
 // Elements used to show and remove the help screen
 const helpBtn = document.querySelector('#helpBtn');
@@ -67,22 +68,116 @@ const fillBoxes = count => {
     winningBox = colorBoxes[winningBox];
 }
 
+// Used to set the pointer event to the given type
+// For my project it is usually either none or auto
+const setPointerEvent = (elements, type) => {
+    for(const element of elements)
+    {
+        element.style.pointerEvents = type;
+    }
+}
+
 // Giving each box and event listener to be able to determine did the user win or not
 colorBoxes.forEach( box => {
     box.addEventListener('click', event => {
+        setPointerEvent(colorBoxes, 'none');
+
         if( event.target === winningBox )
         {
             streakCount++;
+            animateGood();
+            animateWBox();
         }
         else
         {
             streakCount = 0;
+            animateBad();
+            animateLBox(box);
+            animateWBox();
         }
         streak.innerHTML = streakCount;
-
-        fillBoxes(6);
     });
 });
 
 // Used to create the first game
 fillBoxes(6);
+
+// Element that represents the streak message
+const streakMsg = document.querySelector('body div div p');
+
+// Used to make the streak message animate in a "congrats on your success" sort of way
+const animateGood = () => {
+    streakMsg.style.pointerEvents = 'none';
+
+    anime({
+        targets: streakMsg,
+        translateY: -30,
+        scale: 1.1,
+        color: 'hsl(140, 80%, 50%)',
+        direction: 'reverse',
+        duration: 1000,
+        easing: 'easeInOutSine'
+    });
+}
+
+// Used to make the winning box animate in a "congrats on your success" sort of way
+const animateWBox = () => {
+    anime({
+        targets: winningBox,
+        scale: [
+            { value: 1.2, duration: 1000},
+            { value: 1, duration: 0}
+        ],
+        borderColor: [
+            { value: 'hsl(140, 80%, 50%)', duration: 1000},
+            { value: 'hsl(140, 80%, 100%)', duration: 0}
+        ],
+        complete: () => {
+            streakMsg.style.pointerEvents = 'auto';
+
+            setPointerEvent(colorBoxes, 'auto');
+            fillBoxes(6);
+        },
+        easing: 'easeInOutSine'
+    });
+}
+
+// Used to make the losing box animate in a "you made a mistake" sort of way
+const animateLBox = box => {
+    anime({
+        targets: box,
+        translateX: [
+            { value: 10, duration: 250},
+            { value: -10, duration: 250},
+            { value: 10, duration: 250},
+            { value: 0, duration: 0}
+        ],
+        borderColor: [
+            { value: 'hsl(0, 80%, 50%)', duration: 1000},
+            { value: 'hsl(140, 80%, 100%)', duration: 0}
+        ],
+        easing: 'easeInOutSine'
+    });
+}
+
+// Used to make the streak message animate in a "you made a mistake" sort of way
+const animateBad = () => {
+    streakMsg.style.pointerEvents = 'none';
+
+    anime({
+        targets: streakMsg,
+        translateY: [
+            { value: 30, duration: 800},
+            { value: 0, duration: 0}
+        ],
+        rotate: [
+            { value: 10, duration: 800},
+            { value: 0, duration: 0}
+        ],
+        color: [
+            { value: 'hsl(0, 80%, 50%)', duration: 800},
+            { value: 'hsl(0, 80%, 100%)', duration: 0}
+        ],
+        easing: 'easeInOutSine'
+    });
+}
